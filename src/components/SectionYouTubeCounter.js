7722422d734b6783e269f85a170ed2da8a7a9124
Stackbit Code Editor <projects@stackbit.com>
2021-04-24 21:29:35 +0000

    SectionYouTubeCounter.js: changed

--- src/components/SectionYouTubeCounter.js
+++ src/components/SectionYouTubeCounter.js
@@ -8,19 +8,7 @@ export default class SectionYouTubeCounter extends React.Component {
         let section = _.get(this.props, 'section', null);
         return (
             <section id={_.get(section, 'section_id', null)} className="youtubecounter">
-              {_.get(section, 'title', null) && (
-              <h1 className="block-title inner-sm">{_.get(section, 'title', null)}</h1>
-              )}
-              {_.get(section, 'content', null) && (
-              <div className="block-content inner-sm">
-                {markdownify(_.get(section, 'content', null))}
-              </div>
-              )}
-              {_.get(section, 'actions', null) && (
-              <div className="block-buttons inner-sm">
-                <CtaButtons {...this.props} actions={_.get(section, 'actions', null)} />
-              </div>
-              )}
+                
             </section>
         );
     }
