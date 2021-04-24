2cc201b40c9a309cdb5a31f9e7a5bab85d71cffe
Stackbit Code Editor <projects@stackbit.com>
2021-04-24 21:30:30 +0000

    index.js: changed

--- src/components/index.js
+++ src/components/index.js
@@ -8,6 +8,7 @@ import SectionContent from './SectionContent';
 import SectionForm from './SectionForm';
 import SectionHero from './SectionHero';
 import SectionPosts from './SectionPosts';
+import SectionPosts from './SectionPosts';
 
 export {
     CtaButtons,
